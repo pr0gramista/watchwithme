2b71edb3e227e4de0281a4f93853a88306366d1d
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-06-29 22:55:41 +0200

    Add base template, CSS

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/room')
 def rooms():
-    return 'Roooms'
+    return 'Rooms'
+
 
 @app.route('/room/<room_id>')
 def single_room(room_id):
-    return 'This is room %s' % room_id
+    return render_template('room.html', room_id=room_id)
