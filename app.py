215e60e09c071653689092f3de0cfc37c2fdbcd8
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-07-01 20:21:45 +0200

    Add time recovery when joining room

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, render_template, request, make_response, redirect
 from flask_socketio import SocketIO, join_room, leave_room
-import random, string, sys
+import random, string, sys, time
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -13,21 +13,27 @@ def get_unique(length):
     return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(length))
 
 @socketio.on('join')
-def handle_pause_at(room):
+def handle_join(room):
     if room in rooms:
         print("User joined %s" % room)
         join_room(room)
 
 @socketio.on('pauseAt')
-def handle_pause_at(room, time):
-    print('Pause: ' + str(time))
-    socketio.emit('pause', time, room=room, include_self=False)
+def handle_pause_at(room, t):
+    print('Pause: ' + str(t))
+    rooms[room]['video_time'] = t
+    rooms[room]['last_update'] = time.time()
+    rooms[room]['video_state'] = 'pause'
+    socketio.emit('pause', t, room=room, include_self=False)
 
 
 @socketio.on('playAt')
-def handle_play_at(room, time):
-    print('Play: ' + str(time))
-    socketio.emit('play', time, room=room, include_self=False)
+def handle_play_at(room, t):
+    print('Play: ' + str(t))
+    rooms[room]['video_time'] = t
+    rooms[room]['last_update'] = time.time()
+    rooms[room]['video_state'] = 'play'
+    socketio.emit('play', t, room=room, include_self=False)
 
 
 def get_unique_room_id():
@@ -48,8 +54,10 @@ def index():
     room = {
         'owner': username,
         'video_id': 'feA64wXhbjo',
+        'video_state': 'pause',
         'video_set_permission': 'all',
-        'video_time': 30,
+        'video_time': 0,
+        'last_update': time.time()
     }
     rooms[room_id] = room
 
@@ -60,6 +68,12 @@ def index():
 
 @app.route('/room/<room_id>')
 def single_room(room_id):
+    if rooms[room_id]['video_state'] == 'play':
+        t = rooms[room_id]['video_time'] + time.time() - rooms[room_id]['last_update']
+    else:
+        t = rooms[room_id]['video_time']
+
     return render_template('room.html',
         room_id=room_id,
+        time=t,
         room=rooms[room_id])
