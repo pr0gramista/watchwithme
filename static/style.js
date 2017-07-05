885f78656e7de60a2199c65dbd21af28c8d906e6
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-07-05 18:45:48 +0200

    Modal can be used to set username

--- static/style.js
+++ static/style.js
@@ -2,3 +2,18 @@ $('.tab').click(function (event) {
   $('.tab').removeClass('active')
   $(event.currentTarget).addClass('active')
 })
+
+$('.modal').click(function () {
+  console.log(event);
+  if (event.target.className == 'modal-content-wrapper') {
+    $(event.target).parent().remove()
+  }
+})
+
+$('#wwm-name-button').click(function () {
+  var user = $("#wwm-name").val()
+  if (user.length > 0) {
+    Cookies.set("wwm-user", user)
+    $('#wwm-name-modal').remove()
+  }
+})
