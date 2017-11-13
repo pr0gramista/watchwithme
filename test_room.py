740f5ddd639883e00aa718c093d331d5fe118e98
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-11-13 23:14:12 +0100

    Fix Travis, tests doesn't require API key

--- test_room.py
+++ test_room.py
@@ -1,10 +1,19 @@
 import app
 import room
+import test_data
 
 r = room.Room('this2is6unique')
 
 
 def test_is_playlist_added():
+    def mock_get_playlist_info(playlist_id):
+        return test_data.playlist_info
+
+    def mock_get_playlist_items(playlist_id):
+        return test_data.playlist_items
+
+    app.yt.get_playlist_items = mock_get_playlist_items
+    app.yt.get_playlist_info = mock_get_playlist_info
     r.import_yt_playlist('PL3hSzXlZKYpM8XhxS0v7v4SB2aWLeCcUj')
     assert len(r.playlists) == 1
 
@@ -12,3 +21,4 @@ def test_is_playlist_added():
 def test_is_playlist_removed():
     r.remove_playlist('PL3hSzXlZKYpM8XhxS0v7v4SB2aWLeCcUj')
     assert len(r.playlists) == 0
+
