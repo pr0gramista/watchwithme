dbe2f05f22b056e1f255596c750190b169333efc
Bartosz Wiśniewski <poprosturonin@gmail.com>
2018-01-15 22:44:32 +0100

    Fix #8, missing playlist properties

--- src/playlist.py
+++ src/playlist.py
@@ -7,7 +7,7 @@ from .video import VideoState
 class Playlist:
     def __init__(self, playlist_id):
         self.id = playlist_id
-        vars().update(yt.get_playlist_info(playlist_id))
+        self.__dict__.update(yt.get_playlist_info(playlist_id))
         self.videos = yt.get_playlist_items(playlist_id)
 
         self.video_time = 0
